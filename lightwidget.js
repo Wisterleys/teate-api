b82ed8624378db49455f032d7abbab26e34004c2
Wisterleys <wister.sousa2@gmail.com>
2022-03-12 10:16:12 -0300

    console

--- lightwidget.js
+++ lightwidget.js
@@ -5,6 +5,7 @@
     (t.lightwidget={},g=null,t.addEventListener("message",function(t){
         var teste =t.origin.replace('https','http')
         //t.origin = t.origin.replace('https','http')
+        console.log(teste)
         if(-1!==d.indexOf(teste.replace(/^https?:\/\//i,""))){
             var e="object"==typeof t.data?t.data:JSON.parse(t.data);if("lightwidget_lightbox"===e.type&&null===g)return(g=i.createElement("script")).src="https://cdn.lightwidget.com/widgets/lightwidget-lightbox.y.js".replace("y",e.version),void i.body.appendChild(g);e.size<=0||[].forEach.call(i.querySelectorAll('iframe[src*="lightwidget.com/widgets/x"],iframe[data-src*="lightwidget.com/widgets/x"],iframe[src*="instansive.com/widgets/x"]'.replace(/x/g,e.widgetId)),function(t){t.style.height=e.size+"px"})
         }
